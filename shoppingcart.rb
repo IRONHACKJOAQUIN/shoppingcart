57e5f53e048f41e0af89449bbe10b8c5dd525058
IRONHACKJOAQUIN <ironhackjoaquin@gmail.com>
2016-07-25 00:25:51 +0200

    Carro compra terminado pero me da un error

--- shoppingcart.rb
+++ shoppingcart.rb
@@ -4,13 +4,28 @@
 #Clase Carro
 class CarritoCompra 
 	def initialize 
-		@dia_compra = [L,M,X,J,V,S]
-		@articulos = [Articulo] #array de articulos que tendremos en nuestro carro
+		#@dia_compra = [L,M,X,J,V,S]
+		#@articulos = [Articulo] #array de articulos que tendremos en nuestro carro
+
+		@lineas = []
 	end
+
+	def add_linea(linea)
+		@items.push(item)
+	end
+
+	def revisioncompra
+		importe_total = 0
+		@items.each do |item|
+		importe_total += item.precio
+	end
+	#puts "El importe total de su compra: " importe_total
 
 end
 
-#compra01 = CarritoCompra.new "L", bananas, orange,
+
+
+#carro01 = CarritoCompra.new [bananas, 1, "L"]  #Con esto seria suficiente precio, dia compra
 
 
 
@@ -26,11 +41,15 @@ class Articulo
 		@familia = "NA"
 
 	end
+	#funcion que devuelve el precio. 
+	def precio
+		@precio
+	end
 
 	def describe_articulo
 		puts "El articulo #{@descripcion} tiene un precio de #{@precio} . Es de la Familia #{@familia} "
 	end
-	#def precio
+	
 
 	#end
 end
@@ -42,6 +61,14 @@ class ArticuloHogar <Articulo
 	@precio = precio
 	@familia = "Hogar" 
 	end
+
+	def precio
+		if @precio >100
+			@precio = @precio +(@precio * 0.05)
+			@precio 
+		else
+			@precio
+	end
 end
 
 class ArticuloFruteria <Articulo
@@ -49,28 +76,36 @@ class ArticuloFruteria <Articulo
 	@descripcion = descripcion
 	@precio = precio
 	@familia = "Frutas"
-
-	#@familia = "Frutas"
-	#@familia = familia
+	#@diaventa = Date.today  tendre que pasar a string?
 	end
 
-	
+	def precio
+		require 'date'
+		hoy = Date.today
+		if hoy.saturday? || today.sunday?
+			@precio = precio -(precio *0.10)
+			@precio 
+		else 
+			@precio 
+		end	
+	end
 
+	def diaquesevende
+		puts "El articulo #{@descripcion} se ha vendido en dia#{@diaventa}"
+	end
 end
 
+end
 
 #Creando los articulos segun el ejercicios . despues probar metiendo los articulos en un array. 
 
 lista = 
 [
-
-(bananas = ArticuloFruteria.new "Bananas", 10),
-(orange = ArticuloFruteria.new "Orange Juice", 10),
-(rice = ArticuloFruteria.new "Rice", 1),
-(vaccum = ArticuloHogar.new "Vaccum Cleaner", 150),
-(anchovies = Articulo.new "Anchoas of Cantabric", 2)
-
-
+(bananasl = ArticuloFruteria.new "Bananas", 10),
+(orangel = ArticuloFruteria.new "Orange Juice", 10),
+(ricel = ArticuloFruteria.new "Rice", 1),
+(vaccuml = ArticuloHogar.new "Vaccum Cleaner", 150),
+(anchoviesl = Articulo.new "Anchoas of Cantabric", 2)
 ]
 
 
@@ -79,6 +114,16 @@ orange = ArticuloFruteria.new "Orange Juice", 10
 rice = ArticuloFruteria.new "Rice", 1
 vaccum = ArticuloHogar.new "Vaccum Cleaner", 150
 anchovies = Articulo.new "Anchoas of Cantabric", 2
+
+platanodomingo = ArticuloFruteria.new "Platano Domingo", 12
+platanodomingo.diaquesevende
+
+
+carro01 = CarritoCompra.new
+carro01.add_linea(bananas)
+carro01.add_linea(orange)
+carro01.add_linea(vaccum)
+carro01.revisioncompra
 
 
 #bananas.describe_articulo
@@ -104,5 +149,4 @@ ref3.describe_articulo  # aqui igual con la fruta
 
 =end 
 
-
-
+end
